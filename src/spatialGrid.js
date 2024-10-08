c61cd88aaa7d8bcdccd0edb5a86c954ee3d02b1c
Juan-LukeKlopper <jlklopper@protonmail.com>
2024-10-08 15:55:34 +0200

    feat: add retrieve method to get objects from grid

--- src/spatialGrid.js
+++ src/spatialGrid.js
@@ -28,6 +28,25 @@ class SpatialGrid {
 		}
 	}
 
+	retrieve(minX, minY, maxX, maxY) {
+		const results = new Set();
+
+		for (let x = Math.floor(minX / this.cellSize); x <= Math.floor(maxX / this.cellSize); x++) {
+			for (let y = Math.floor(minY / this.cellSize); y <= Math.floor(maxY / this.cellSize); y++) {
+				const key = `${x},${y}`;
+				if (this.grid[key]) {
+					this.grid[key].forEach(obj => results.add(obj));
+				}
+			}
+		}
+
+		// Filter objects that don't fully fit inside the area
+		return Array.from(results).filter(obj => {
+			const bounds = obj.getBounds();
+			return bounds.minX >= minX && bounds.maxX <= maxX &&
+				bounds.minY >= minY && bounds.maxY <= maxY;
+		});
+	}
 
 
 }
