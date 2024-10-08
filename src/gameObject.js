33f08c69d41c524638be55bd22c5ae3ada22487b
Juan-LukeKlopper <jlklopper@protonmail.com>
2024-10-08 15:40:39 +0200

    feat: add basic GameObject class with pos and size

---
+++ src/gameObject.js
@@ -0,0 +1,11 @@
+class GameObject {
+	constructor(x, y, width = 1, height = 1) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+}
+
+module.exports = GameObject;
+
