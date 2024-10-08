c4dac3b524995095376b78c805644630d12e88a8
Juan-LukeKlopper <jlklopper@protonmail.com>
2024-10-08 15:44:38 +0200

    feat: make app more failure proof by adding checks to size

--- src/gameObject.js
+++ src/gameObject.js
@@ -1,5 +1,8 @@
 class GameObject {
 	constructor(x, y, width = 1, height = 1) {
+		if (width <= 0 || height <= 0) {
+			throw new Error('Width and height must be positive values.');
+		}
 		this.x = x;
 		this.y = y;
 		this.width = width;
@@ -17,4 +20,3 @@ class GameObject {
 }
 
 module.exports = GameObject;
-
